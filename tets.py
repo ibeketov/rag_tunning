b5ad02bda3c91c781c02ed03dd26f020dcdccc61
Ilya Beketov <ilya.beketov@gmail.com>
2024-01-06 19:08:17 +0100

    Initial code commit

---
+++ tets.py
@@ -0,0 +1,9 @@
+import os
+import openai
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
+print(openai.api_key)
